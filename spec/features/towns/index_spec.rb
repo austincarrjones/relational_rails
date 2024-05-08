ccc996faa385fca9ba25f5e2273ef7f00a47c44f
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-05-08 11:57:15 -0600

    initial commit

---
+++ spec/features/towns/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "towns index page" do
+  #   [ ] done
+
+  # User Story 1, Parent Index 
+
+  # For each parent table
+  # As a visitor
+  # When I visit '/parents'
+  # Then I see the name of each parent record in the system
+
+  context "As a User" do
+    describe "when I visit towns index" do
+      it "I can see the name of each town record" do
+        
+      end
+  end
+end
