611f0a4bcfc5e2f174d4d89506146e54602d1822
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-05-09 14:38:55 -0600

    feat: user story 1

--- spec/features/towns/index_spec.rb
+++ spec/features/towns/index_spec.rb
@@ -1,21 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "towns index page" do
-  #   [ ] done
+#   [ ] done
   # User Story 1, Parent Index 
   # For each parent table
   # As a visitor
   # When I visit '/parents'
   # Then I see the name of each parent record in the system
 
+RSpec.describe "towns index page" do
   context "As a User" do
     describe "when I visit towns index" do
       it "I can see the name of each town record" do
+        breck = Town.create!(name: "Breckenridge")
+        frisco = Town.create!(name: "Frisco")
+
         visit "/towns"
-        expect(page).to have_content("Towns")
+        # save_and_open_page
         expect(page).to have_content("Breckenridge")
         expect(page).to have_content("Frisco")
-        expect(page).to_not have_content("NYC")
       end
     end
   end
