f6f520365f11fc33192f2815391caddf299a166b
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-05-08 21:00:15 -0600

    feat: town index

--- spec/features/towns/index_spec.rb
+++ spec/features/towns/index_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "towns index page" do
   #   [ ] done
-
   # User Story 1, Parent Index 
-
   # For each parent table
   # As a visitor
   # When I visit '/parents'
@@ -13,7 +11,12 @@ RSpec.describe "towns index page" do
   context "As a User" do
     describe "when I visit towns index" do
       it "I can see the name of each town record" do
-        
+        visit "/towns"
+        expect(page).to have_content("Towns")
+        expect(page).to have_content("Breckenridge")
+        expect(page).to have_content("Frisco")
+        expect(page).to_not have_content("NYC")
       end
+    end
   end
 end
