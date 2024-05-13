f737ce67f10dda8ff996a7498374ef2ce7ad3612
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-05-12 20:06:34 -0600

    add seeds

--- app/controllers/towns_controller.rb
+++ app/controllers/towns_controller.rb
@@ -2,11 +2,9 @@ class TownsController < ApplicationController
 
   def index
     @towns = Town.all.order(created_at: :asc)
-    #binding.pry
   end
 
   def show
     @town = Town.find(params[:id])
-    # binding.pry
   end
 end
